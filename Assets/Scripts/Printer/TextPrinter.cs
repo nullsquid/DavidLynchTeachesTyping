cb9437c2cfbe10f409c21c97c370592831bc52df
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-10-19 00:33:54 -0400

    Added textmesh pro and started on main screen UI

---
+++ Assets/Scripts/Printer/TextPrinter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class TextPrinter : MonoBehaviour {
+	public TextMeshProUGUI printText;
+	public string textToPrint;
+
+	#region Singleton
+	public static TextPrinter instance;
+	void Awake() {
+
+		if (instance == null) {
+			instance = this;
+		}
+		else if (instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+	#endregion
+
+	public void InvokePrint(string text, float time){
+		textToPrint = text;
+		StartCoroutine (PrintText (time));
+	}
+
+	IEnumerator PrintText(float timeBtwChars){
+		for (int i = 0; i < textToPrint.Length; i++) {
+			printText.text += textToPrint [i];
+			yield return new WaitForSeconds (timeBtwChars);
+		}
+	}
+
+}
---
+++ Assets/Scripts/StageManager/StageManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour {
+
+	#region Singleton
+	public static StageManager instance;
+	void Awake() {
+
+		if (instance == null) {
+			instance = this;
+		}
+		else if (instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+	#endregion
+
+	void Start(){
+		TextPrinter.instance.InvokePrint ("press any key to begin", 0.05f);
+	}
+
+	public void StartStage(int stageNumber){
+		switch (stageNumber) {
+		case 1:
+			break;
+
+		default:
+			break;
+		}
+	}
+}
