54b6065c2336ddd5c48f8a61ae976a859f00a4c2
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-10-27 12:58:58 -0400

    Added multiple sprites and animations to all lynch portraits

--- Assets/Scripts/StageManager/Stages/Stage_1.cs
+++ Assets/Scripts/StageManager/Stages/Stage_1.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 public class Stage_1 : Stage {
-
+    public Animator animator;
+    float talkLength;
 	public void Start(){
 		TextPrinter.instance.onPrintComplete += EndStage;
 	}
@@ -15,13 +16,18 @@ public class Stage_1 : Stage {
 
 	public override void StartStage(){
 		TextPrinter.instance.printText = GameObject.Find ("MainText_1").GetComponent<TextMeshProUGUI>();
-		TextPrinter.instance.InvokePrint ("Hello, this is film maker David Lynch....I'm going to be taking you through the magical\nworld of typing. By the time you've finished this computer program.... you'll be a typing wizard. Let's get started.\n\npress any key to continue", 0.08f);
+        animator.SetBool("IsTalking", true);
+        TextPrinter.instance.InvokePrint ("Hello, this is film maker David Lynch....I'm going to be taking you through the magical\nworld of typing. By the time you've finished this computer program.... you'll be a typing wizard. Let's get started.\n\npress any key to continue", 0.08f);
         GameObject.FindObjectOfType<DialogueAudioHandler>().InvokeSoundEffect("STAGE_1");
+        
 
     }
-
+    void StopTalking() {
+        animator.SetBool("IsTalking", false);
+    }
     public override void EndStage(){
 		stageIsComplete = true;
+        animator.SetBool("IsTalking", false);
 	}
     public Image blackSolid;
     float t = 0;
