a87251cbf25d063311575e2cee2326e68f9f81fc
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2017-11-17 12:07:51 -0500

    Added new UI elements for stage 2

--- Assets/Scripts/StageManager/Stages/Stage_2.cs
+++ Assets/Scripts/StageManager/Stages/Stage_2.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class Stage_2 : Stage {
 	public Animator animator;
 	bool blink = true;
+    public ScrollRect scrollrect;
 	public void OnEnable(){
         if (TextPrinter.instance != null) {
             TextPrinter.instance.onPrintComplete += EndStage;
@@ -44,13 +46,18 @@ public class Stage_2 : Stage {
         animator.SetBool("IsTalking", true);
     }
 	IEnumerator TextBlink(){
-		while (blink == true) {
+        
+
+        while (blink == true) {
 			//if (!TextPrinter.instance.printText.text.Contains ("<color=yellow>(press any key to continue)</color>")) {
 			TextPrinter.instance.printText.text += "<color=yellow>(press any key to continue)</color>";
+            //scrollrect.enabled = false;
 			yield return new WaitForSeconds (0.5f);
-			//} else {
-			TextPrinter.instance.printText.text = TextPrinter.instance.printText.text.Replace ("<color=yellow>(press any key to continue)</color>", string.Empty);
-			yield return new WaitForSeconds (0.5f);
+            //} else {
+            scrollrect.enabled = false;
+            TextPrinter.instance.printText.text = TextPrinter.instance.printText.text.Replace ("<color=yellow>(press any key to continue)</color>", string.Empty);
+            
+            yield return new WaitForSeconds (0.5f);
 			//}
 
 		}
