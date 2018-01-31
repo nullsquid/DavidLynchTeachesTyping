122dde248115af4ccf72cfb633fefd8693ad7b4e
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2018-01-31 09:52:42 -0500

    Rebuilt stage 9 minigame, wired it into existing game

--- Assets/Scripts/StageManager/Stages/Stage_9.cs
+++ Assets/Scripts/StageManager/Stages/Stage_9.cs
@@ -5,6 +5,9 @@ using TMPro;
 using UnityEngine.UI;
 public class Stage_9 : Stage {
     public ScrollRect scrollrect;
+    public Scrollbar mainScrollBar;
+    public Scrollbar gameScrollBar;
+    public GameObject minigamePanel;
 	public GameObject lHand;
 	public GameObject rHand;
     public GameObject seperator;
@@ -15,8 +18,10 @@ public class Stage_9 : Stage {
     public GameObject FPrefab;
     public GameObject playSpace;
 	public Animator animator;
+    public List<string> sequence = new List<string>();
     public List<GameObject> JList = new List<GameObject>();
     public List<GameObject> FList = new List<GameObject>();
+    float t = 0;
 	public void OnEnable() {
 		if (TextPrinter.instance != null) {
 			TextPrinter.instance.onPrintComplete += EndStage;
@@ -33,10 +38,7 @@ public class Stage_9 : Stage {
 
     public override void StartStage() {
         seperator.SetActive(false);
-        for(int i = 0; i < 3; i++) {
-            FList[i].SetActive(false);
-            JList[i].SetActive(false);
-        }
+        
         TextPrinter.instance.printText = GameObject.Find("MainText_9").GetComponent<TextMeshProUGUI>();
         StartCoroutine(StartMinigame());
 
@@ -46,115 +48,44 @@ public class Stage_9 : Stage {
     public override void EndStage() {
         stageIsComplete = true;
     }
-
+    IEnumerator AnimateGame(float curValue) {
+        t = 0;
+        while(t < curValue + 0.05f) {
+            t += Time.deltaTime * 1f;
+            float newValue = Mathf.Lerp(gameScrollBar.value, gameScrollBar.value += 0.05f, t);
+            gameScrollBar.value = newValue;
+        }
+        yield return null;
+    }
     void Update() {
-        if(Input.GetKeyDown(KeyCode.F) && ((GameObject.Find("FKey0") != null && GameObject.Find("FKey0").activeSelf == true) || (GameObject.Find("FKey1") != null && GameObject.Find("FKey1").activeSelf == true) || (GameObject.Find("FKey2") != null && GameObject.Find("FKey2").activeSelf == true) || (GameObject.Find("FKey3") != null && GameObject.Find("FKey3").activeSelf == true) || (GameObject.Find("FKey4") != null && GameObject.Find("FKey4").activeSelf == true) || (GameObject.Find("FKey5") != null && GameObject.Find("FKey5").activeSelf == true) || (GameObject.Find("FKey6") != null && GameObject.Find("FKey6").activeSelf == true) || (GameObject.Find("FKey7") != null && GameObject.Find("FKey7").activeSelf == true) || (GameObject.Find("FKey8") != null && GameObject.Find("FKey8").activeSelf == true) || (GameObject.Find("FKey9") != null && GameObject.Find("FKey9").activeSelf == true) || (GameObject.Find("FKey10") != null && GameObject.Find("FKey10").activeSelf == true) || (GameObject.Find("FKey11") != null && GameObject.Find("FKey11").activeSelf == true))) {
-            if(GameObject.Find("FKey0") != null && GameObject.Find("FKey0").activeSelf == true) {
-                Destroy(GameObject.Find("FKey0"));
-            }
-            else if (GameObject.Find("FKey1") != null && GameObject.Find("FKey1").activeSelf == true) {
-                Destroy(GameObject.Find("FKey1"));
-            }
-            else if (GameObject.Find("FKey2") != null && GameObject.Find("FKey2").activeSelf == true) {
-                Destroy(GameObject.Find("FKey2"));
-            }
-            else if (GameObject.Find("FKey3") != null && GameObject.Find("FKey3").activeSelf == true) {
-                Destroy(GameObject.Find("FKey3"));
-            }
-            else if (GameObject.Find("FKey4") != null && GameObject.Find("FKey4").activeSelf == true) {
-                Destroy(GameObject.Find("FKey4"));
-            }
-            else if (GameObject.Find("FKey5") != null && GameObject.Find("FKey5").activeSelf == true) {
-                Destroy(GameObject.Find("FKey5"));
-            }
-            else if (GameObject.Find("FKey6") != null && GameObject.Find("FKey6").activeSelf == true) {
-                Destroy(GameObject.Find("FKey6"));
-            }
-            else if (GameObject.Find("FKey7") != null && GameObject.Find("FKey7").activeSelf == true) {
-                Destroy(GameObject.Find("FKey7"));
-            }
-            else if (GameObject.Find("FKey8") != null && GameObject.Find("FKey8").activeSelf == true) {
-                Destroy(GameObject.Find("FKey8"));
-            }
-            else if (GameObject.Find("FKey9") != null && GameObject.Find("FKey9").activeSelf == true) {
-                Destroy(GameObject.Find("FKey9"));
-            }
-            else if (GameObject.Find("FKey10") != null && GameObject.Find("FKey10").activeSelf == true) {
-                Destroy(GameObject.Find("FKey10"));
-            }
-            else if (GameObject.Find("FKey11") != null && GameObject.Find("FKey11").activeSelf == true) {
-                Destroy(GameObject.Find("FKey11"));
-            }
+        if(Input.GetKeyDown(KeyCode.F) && sequence[0] == "F") {
+            gameScrollBar.value = gameScrollBar.value + 0.1f;
+            sequence.RemoveAt(0);
         }
-        if (Input.GetKeyDown(KeyCode.J) && ((GameObject.Find("JKey0") != null && GameObject.Find("JKey0").activeSelf == true) || (GameObject.Find("JKey1") != null && GameObject.Find("JKey1").activeSelf == true) || (GameObject.Find("JKey2") != null && GameObject.Find("JKey2").activeSelf == true) || (GameObject.Find("JKey3") != null && GameObject.Find("JKey3").activeSelf == true) || (GameObject.Find("JKey4") != null && GameObject.Find("JKey4").activeSelf == true) || (GameObject.Find("JKey5") != null && GameObject.Find("JKey5").activeSelf == true) || (GameObject.Find("JKey6") != null && GameObject.Find("JKey6").activeSelf == true) || (GameObject.Find("JKey7") != null && GameObject.Find("JKey7").activeSelf == true) || (GameObject.Find("JKey8") != null && GameObject.Find("JKey8").activeSelf == true) || (GameObject.Find("JKey9") != null && GameObject.Find("JKey9").activeSelf == true) || (GameObject.Find("JKey10") != null && GameObject.Find("JKey10").activeSelf == true) || (GameObject.Find("JKey11") != null && GameObject.Find("JKey11").activeSelf == true))) {
-            if (GameObject.Find("JKey0") != null && GameObject.Find("JKey0").activeSelf == true) {
-                Destroy(GameObject.Find("JKey0"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey1") != null && GameObject.Find("JKey1").activeSelf == true) {
-                Destroy(GameObject.Find("JKey1"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey2") != null && GameObject.Find("JKey2").activeSelf == true) {
-                Destroy(GameObject.Find("JKey2"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey3") != null && GameObject.Find("JKey3").activeSelf == true) {
-                Destroy(GameObject.Find("JKey3"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey4") != null && GameObject.Find("JKey4").activeSelf == true) {
-                Destroy(GameObject.Find("JKey4"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey5") != null && GameObject.Find("JKey5").activeSelf == true) {
-                Destroy(GameObject.Find("JKey5"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey6") != null && GameObject.Find("JKey6").activeSelf == true) {
-                Destroy(GameObject.Find("JKey6"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey7") != null && GameObject.Find("JKey7").activeSelf == true) {
-                Destroy(GameObject.Find("JKey7"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey7") != null && GameObject.Find("JKey7").activeSelf == true) {
-                Destroy(GameObject.Find("JKey7"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey8") != null && GameObject.Find("JKey8").activeSelf == true) {
-                Destroy(GameObject.Find("JKey8"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey9") != null && GameObject.Find("JKey9").activeSelf == true) {
-                Destroy(GameObject.Find("JKey9"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey10") != null && GameObject.Find("JKey10").activeSelf == true) {
-                Destroy(GameObject.Find("JKey10"));
-                //points += 1;
-            }
-            else if (GameObject.Find("JKey11") != null && GameObject.Find("JKey11").activeSelf == true) {
-                Destroy(GameObject.Find("JKey11"));
-                //points += 1;
-            }
+        if (Input.GetKeyDown(KeyCode.J) && sequence[0] == "J") {
+            gameScrollBar.value = gameScrollBar.value + 0.1f;
+            sequence.RemoveAt(0);
         }
+        if(sequence.Count == 0) {
+            minigamePanel.SetActive(false);
+            
+            StartCoroutine(Evaluation());
+        }
+        //remove once done
+        
+        
     }
 
     IEnumerator StartMinigame() {
 		animator.SetBool ("IsTalking", true);
         TextPrinter.instance.InvokePrint("<Okay, ;0.06>{1.5}<We are now going ;0.06>{.6}<to assess your speed. ;0.06>{.6}<Are you ready? ;0.06>", 0.08f);
         GameObject.FindObjectOfType<DialogueAudioHandler>().InvokeSoundEffect("STAGE_9");
-        yield return new WaitForSeconds(10f);
+        yield return new WaitForSeconds(8f);
+        animator.SetBool("IsTalking", false);
         mainText.SetActive(false);
         seperator.SetActive(true);
-        StartCoroutine(Minigame());
+        minigamePanel.SetActive(true);
 
-    }
-
-    void MiniGameStart() {
-        //StartCoroutine(Minigame());
     }
 
 
@@ -167,67 +98,13 @@ public class Stage_9 : Stage {
 		animator.SetBool("IsTalking", true);
 	}
 
-    IEnumerator Minigame() {
-		animator.SetBool ("IsTalking", false);
-		rHand.SetActive (false);
-		lHand.SetActive (false);
-        FList[0].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[0].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[1].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[1].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[2].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[2].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        ////////////////////////
-        FList[3].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[3].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[4].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[4].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[5].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[5].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        ////////////////////////
-        FList[6].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[6].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[7].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[7].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[8].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[8].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        /////////////////////////
-        FList[9].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[9].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[10].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[10].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        FList[11].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        JList[11].SetActive(true);
-        yield return new WaitForSeconds(Random.Range(.5f, 2f));
-        StartCoroutine(Evaluation());
-    }
+    
     IEnumerator Evaluation() {
         seperator.SetActive(false);
         mainText.SetActive(true);
         portrait.SetActive(true);
+        mainScrollBar.value = 0;
+        TextPrinter.instance.ClearText();
         TextPrinter.instance.InvokePrint("\n\nEvaluating...", 0.1f);
         yield return new WaitForSeconds(1.5f);
         StageManager.instance.StartStage(10);
